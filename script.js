7a50e9ac0bfe646b68eba50c545036457b34698b
alfonsodcf <alfonsoservizi01@gmail.com>
2022-04-10 21:46:59 +0200

    script:click

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-let parola = prompt("inseisci parola");
 
 
-function giraparola () {
-    let letters = parola.split("");
-    console.log(letters);
-    let parolagirata = "";
-    for (let i = letters.length -1; i >= 0; i--){//questo for funziona al contrario cosi da permetermi di legere da lultima lettera del for
-        parolagirata += letters[i];
 
-    
+function inserisciparola (){
+    let parola = prompt("inseisci parola");
+
+    let risultato = document.getElementById('risultato');
+
+
+    function giraparola () {
+        let fixmaiuscole = parola.toLowerCase();
+        let letters = fixmaiuscole.split("");
+        console.log(letters);
+        console.log(fixmaiuscole);
+        let parolagirata = "";
+        for (let i = letters.length -1; i >= 0; i--){//questo for funziona al contrario cosi da permetermi di legere da lultima lettera del for
+            parolagirata += letters[i];
+
+        }
+
+        if(parolagirata == fixmaiuscole){
+            risultato.innerText = (`La parola (${parola}) è palindroma`)
+        } else{
+            risultato.innerText = (`La parola (${parola}) non è palindroma`)
+        }
+        return parolagirata;
     }
-    return parolagirata;
-
-    function parolaPalindroma(parolagirata);
-}
-
-
-function parolaPalindroma(parolagirata){
-    if(parolagirata === parola ){
-        //parola palindroma
-    } else{
-        //parola non palindroma
-    }
-}
-
 console.log(giraparola());
+}
+
+
+
+
+document.getElementById("palindroma").addEventListener("click", inserisciparola);
